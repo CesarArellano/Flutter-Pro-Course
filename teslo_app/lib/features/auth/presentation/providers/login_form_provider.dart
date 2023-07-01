3d569ddc77f51e54e62930005b231cffa8869c9a
Cesar Arellano <raywayday@gmail.com>
2023-06-30 21:04:57 -0600

    Add loginFormProvider and methods to call

--- teslo_app/lib/features/auth/presentation/providers/login_form_provider.dart
+++ teslo_app/lib/features/auth/presentation/providers/login_form_provider.dart
@@ -1,3 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:formz/formz.dart';
+
 import '../../../shared/infrastructured/inputs/inputs.dart';
 
 class LoginFormState {
@@ -41,3 +44,47 @@ class LoginFormState {
     ''';
   }
 }
+
+class LoginFormNotifier extends StateNotifier<LoginFormState> {
+  LoginFormNotifier(): super(LoginFormState());
+
+  void onEmailChange( String value ) {
+    final newEmail = Email.dirty(value);
+    state = state.copyWith(
+      email: newEmail,
+      isValid: Formz.validate([ newEmail, state.password ])
+    );
+  }
+
+  void onPasswordChange( String value ) {
+    final newPassword = Password.dirty(value);
+    state = state.copyWith(
+      password: newPassword,
+      isValid: Formz.validate([ newPassword, state.email ])
+    );
+  }
+
+  void onFormSubmit() {
+    _touchEveryField();
+    if( !state.isValid ) return;
+
+    print('onFormSubmit: ${ state.toString() }');
+  }
+
+  void _touchEveryField() {
+    final email = Email.dirty(state.email.value);
+    final password = Password.dirty(state.password.value);
+    
+    state = state.copyWith(
+      isFormPosted: true,
+      email: email,
+      password: password,
+      isValid: Formz.validate([ email, password ])
+    );
+  }
+}
+
+// Delete last state when the user log out.
+final loginFormProvider = StateNotifierProvider.autoDispose<LoginFormNotifier, LoginFormState>((ref) {
+  return LoginFormNotifier();
+});
